8826d104ac11ebe54129e3dac4f56fb6eaa5391c
Prakash <prakashyadav9576@gmail.com>
2025-01-30 23:36:42 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+app.set('view engine', 'ejs')
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get('/', (req,res)=>{
+    res.render('Home')
+})
+
+
+
+app.listen(3000, ()=>{
+    console.log("Server is Running...")
+});
